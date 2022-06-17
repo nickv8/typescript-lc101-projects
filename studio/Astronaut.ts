4138a7709dec463098e90457642812d309ff08f5
Nick Villone <n.villone8@gmail.com>
2022-06-16 20:35:57 -0400

    all doneso

---
+++ studio/Astronaut.ts
@@ -0,0 +1,15 @@
+import { Payload } from './Payload'
+
+export class Astronaut {
+    massKg: number;
+    name: string;
+
+    constructor(massKg: number, name: string) {
+        this.massKg = massKg;
+        this.name = name;
+    }
+
+}
+
+
+
